e1f1bd9a8c4a7cae24ae5297e46ea712431bc761
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-04 23:03:40 +0100

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -4,7 +4,7 @@ class Config:
     JWT_SECRET_KEY = "hatrd_to_guess"
     JWT_ACCESS_TOKEN_EXPIRES = 3600
     
-    
+
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'
