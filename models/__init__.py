e382cba3dc9b66a54bc21187317712710f0f3e6a
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-05 15:04:10 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@ from extensions import db
 from enum import Enum
 from passlib.hash import pbkdf2_sha256 as hasher
 from sqlalchemy import Enum as SQLAlchemyEnum
+import re
 
 
 class Gender(Enum):
@@ -19,6 +20,7 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    user_session = db.relationship("UserSession", backref="user", lazy=True, uselist=False)
 
     def __init__(self, email, password, first_name, last_name, gender):
         self.email = email
@@ -27,8 +29,28 @@ class User(db.Model):
         self.last_name = last_name.lower()
         self.gender = Gender(gender)
 
+    def update_password(self, new_password):
+        self.password = hasher.hash(new_password)
+
+    @staticmethod
+    def validate_email(email):
+        return re.match(r"[^@]+@[^@]+\.[^@]+", email)
+
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+class UserSession(db.Model):
+    __tablename__ = "user_sessions"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    otp = db.Column(db.String(6), nullable=True)
+    token = db.Column(db.String(255), nullable=True)
+    otp_expires_at = db.Column(db.DateTime, nullable=True)
+    token_expires_at = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"<UserSession {self.user_id}>"
 
 
 def create_user(email, password, first_name, last_name, gender):
@@ -43,3 +65,38 @@ def verify_user_login(email, password):
     if not user:
         return False
     return hasher.verify(password, user.password)
+
+
+def create_otp_token(user_id, otp=None, token=None):
+    if otp:
+        user_session = UserSession.query.filter_by(user_id=user_id).first()
+        if user_session:
+            user_session.otp = otp
+            user_session.otp_expires_at = db.func.now() + db.func.interval(10, "minute")
+            db.session.commit()
+        else:
+            user_session = UserSession(user_id=user_id, otp=otp, otp_expires_at=db.func.now() + db.func.interval(10, "minute"))
+            db.session.add(user_session)
+            db.session.commit()
+        return user_session
+    elif token:
+        user_session = UserSession.query.filter_by(user_id=user_id).first()
+        if user_session:
+            user_session.token = token
+            user_session.token_expires_at = db.func.now() + db.func.interval(1, "day")
+            db.session.commit()
+        else:
+            user_session = UserSession(user_id=user_id, token=token, token_expires_at=db.func.now() + db.func.interval(1, "day"))
+            db.session.add(user_session)
+            db.session.commit()
+        return user_session
+    return None
+
+
+def get_user_by_email(email):
+    user = User.query.filter_by(email=email.lower()).first()
+    return user
+
+
+def valid_email(email):
+    return User.validate_email(email)
