fed3ebaf41cbf0d7dca7bb1576f95ec05f1674ab
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-05 21:26:46 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from passlib.hash import pbkdf2_sha256 as hasher
 from sqlalchemy import Enum as SQLAlchemyEnum
 import re
 import datetime
+from utils import hex_uuid
 
 
 class Gender(Enum):
@@ -11,25 +12,59 @@ class Gender(Enum):
     F = "Female"
 
 
+class RelationshipType(Enum):
+    wife = "Wife"
+    ex_wife = "Ex-Wife"
+    husband = "Husband"
+    ex_husband = "Ex-Husband"
+    partner = "Partner"
+    ex_partner = "Ex-Partner"
+    secondary_wife = "Secondary Wife"
+
+
+class ChildType(Enum):
+    son = "Son"
+    daughter = "Daughter"
+    step_son = "Step Son"
+    step_daughter = "Step Daughter"
+    adopted_son = "Adopted Son"
+    adopted_daughter = "Adopted Daughter"
+
+
+class FamilyName(db.Model):
+    __tablename__ = "family_names"
+    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
+    name = db.Column(db.String(50), nullable=False)
+
+
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
     email = db.Column(db.String(50), unique=True, nullable=False)
     first_name = db.Column(db.String(50), nullable=True)
     last_name = db.Column(db.String(50), nullable=True)
     gender = db.Column(SQLAlchemyEnum(Gender))
     is_super_admin = db.Column(db.Boolean, default=False)
-    password = db.Column(db.String(255), nullable=False)
+    img_str = db.Column(db.Text, nullable=True)
+    phone_number = db.Column(db.String(25), nullable=True)
+    password = db.Column(db.Text, nullable=False)
+    family_name = db.Column(db.String(50), db.ForeignKey("family_names.id"), nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     user_session = db.relationship("UserSession", backref="user", lazy=True, uselist=False)
 
-    def __init__(self, email, password, first_name, last_name, gender):
+    def __init__(self, email, password, first_name,
+                 last_name, gender, is_super_admin, img_str=None,
+                 family_name=None, phone_number=None):
         self.email = email
         self.password = hasher.hash(password)
         self.first_name = first_name.lower()
         self.last_name = last_name.lower()
         self.gender = Gender(gender)
+        self.is_super_admin = is_super_admin
+        self.family_name = family_name
+        self.img_str = img_str
+        self.phone_number = phone_number
 
     def update_password(self, new_password):
         self.password = hasher.hash(new_password)
@@ -45,8 +80,8 @@ class User(db.Model):
 
 class UserSession(db.Model):
     __tablename__ = "user_sessions"
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
+    user_id = db.Column(db.String(50), db.ForeignKey("users.id"), nullable=False)
     otp = db.Column(db.String(6), nullable=True)
     token = db.Column(db.String(255), nullable=True)
     otp_expires_at = db.Column(db.DateTime, nullable=True)
@@ -56,8 +91,25 @@ class UserSession(db.Model):
         return f"<UserSession {self.user_id}>"
 
 
-def create_user(email, password, first_name, last_name, gender):
-    user = User(email, password, first_name, last_name, gender)
+class Relationship(db.Model):
+    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
+    person_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    partner_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    relationship_type = db.Column(SQLAlchemyEnum(RelationshipType))
+
+
+class Child(db.Model):
+    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
+    parent_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    child_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    child_type = db.Column(SQLAlchemyEnum(ChildType))
+
+
+def create_user(email, password, first_name, last_name,
+                gender, img_str, is_super_admin=False, family_name=None, phone_number=None):
+    user = User(email=email, password=password, first_name=first_name,
+                last_name=last_name, gender=gender, img_str=img_str,
+                is_super_admin=is_super_admin, family_name=family_name, phone_number=phone_number)
     db.session.add(user)
     db.session.commit()
     return user
