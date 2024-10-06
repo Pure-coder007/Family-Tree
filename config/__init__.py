46b02285ef9817bf82be1cdb7e038d02c9dbe316
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-06 12:46:11 +0100

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
-    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    JWT_ACCESS_TOKEN_EXPIRES = 86400
     
 
 class DevelopmentConfig(Config):
