0b9456302936818a0dfaf7fa5b82389d6cb6b666
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-07 14:56:34 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,11 @@ from flask import jsonify
 from flask_jwt_extended import create_access_token
 import random
 import uuid
+import hmac
+import hashlib
+import base64
+from io import BytesIO
+import time
 
 
 def hex_uuid():
@@ -25,3 +30,29 @@ def return_access_token(identity):
 
 def generate_otp():
     return str(random.randint(100000, 999999))
+
+
+def convert_binary(base64_file):
+    try:
+        print("got here")
+        binary_data = base64.b64decode(base64_file)
+        # Convert binary data to a file-like object
+        file_like = BytesIO(binary_data)
+        print(file_like, "file_like from convert_binary")
+        return file_like
+    except Exception as e:
+        print(e, "error from convert_binary")
+        return None
+
+
+def generate_signature(params_to_sign, api_secret):
+    try:
+        params_to_sign['timestamp'] = int(time.time())
+        sorted_params = '&'.join([f'{k}={params_to_sign[k]}' for k in sorted(params_to_sign)])
+        to_sign = f'{sorted_params}{api_secret}'
+        signature = hmac.new(api_secret.encode('utf-8'), to_sign.encode('utf-8'), hashlib.sha1).hexdigest()
+        print(signature, "signature from generate_signature")
+        return signature
+    except Exception as e:
+        print(e, "error from generate_signature")
+        return None
