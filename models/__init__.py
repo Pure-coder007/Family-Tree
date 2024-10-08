0ce50472ff9da50b3dc4a7e34cea9f3f6b4a0455
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-08 18:48:55 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,6 @@ from utils import hex_uuid
 from datetime import datetime, timedelta
 
 
-
 class Gender(Enum):
     male = "Male"
     female = "Female"
@@ -39,45 +38,29 @@ class Status(Enum):
     deceased = "Deceased"
 
 
-class FamilyName(db.Model):
-    __tablename__ = "family_names"
+class Member(db.Model):
+    __tablename__ = "member"
     id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
-    name = db.Column(db.String(50), nullable=False)
-    users = db.relationship("User", backref="family", lazy=True)
-
-    def __init__(self, name):
-        self.name = name.lower()
-
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
-    email = db.Column(db.String(50), unique=True, nullable=False)
     first_name = db.Column(db.String(50), nullable=True)
     last_name = db.Column(db.String(50), nullable=True)
     gender = db.Column(SQLAlchemyEnum(Gender))
-    is_super_admin = db.Column(db.Boolean, default=False)
     dob = db.Column(db.DateTime, nullable=True)
     status = db.Column(SQLAlchemyEnum(Status), default=Status.alive)
     deceased_at = db.Column(db.DateTime, nullable=True)
     img_str = db.Column(db.Text, nullable=True)
     phone_number = db.Column(db.String(25), nullable=True)
-    password = db.Column(db.Text, nullable=False)
-    family_name = db.Column(db.String(50), db.ForeignKey("family_names.id"), nullable=True)
+    occupation = db.Column(db.String(50), nullable=True)
+    birth_place = db.Column(db.String(150), nullable=True)
+    birth_name = db.Column(db.String(150), nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-    user_session = db.relationship("UserSession", backref="user", lazy=True, uselist=False)
 
-    def __init__(self, email, password, first_name,
-                 last_name, gender, is_super_admin, img_str=None,
-                 family_name=None, phone_number=None, dob=None, status=None, deceased_at=None):
-        self.email = email
-        self.password = hasher.hash(password)
+    def __init__(self, first_name,
+                 last_name, gender, img_str=None,
+                phone_number=None, dob=None, status=None, deceased_at=None):
         self.first_name = first_name.lower()
         self.last_name = last_name.lower()
         self.gender = Gender(gender.title())
-        self.is_super_admin = is_super_admin
-        self.family_name = family_name
         self.img_str = img_str
         self.phone_number = phone_number
         self.dob = dob
@@ -85,102 +68,127 @@ class User(db.Model):
         self.deceased_at = deceased_at or None
 
     def to_dict(self):
-        user_dict = {
+        member_dict = {
             "id": self.id,
-            "email": self.email,
             "first_name": self.first_name.title(),
             "last_name": self.last_name.title(),
             "gender": self.gender.value,
-            "is_super_admin": self.is_super_admin,
             "img_str": self.img_str,
-            "family_name": self.family_name.title() if self.family_name else None,
             "phone_number": self.phone_number,
             "dob": self.dob.strftime("%d-%b-%Y") if self.dob else None,
             "status": self.status.value,
-            "deceased_at": self.deceased_at,
+            "deceased_at": self.deceased_at.strftime("%d-%b-%Y") if self.deceased_at else None,
         }
-        return {key: value for key, value in user_dict.items() if value}
+        return {key: value for key, value in member_dict.items() if value}
 
-    def update_password(self, new_password):
-        self.password = hasher.hash(new_password)
-        db.session.commit()
+    def __repr__(self):
+        return f"<Member {self.last_name}>"
+
+
+class Moderators(db.Model):
+    __tablename__ = "moderators"
+    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
+    fullname = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    is_super_admin = db.Column(db.Boolean, default=False)
+    password = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(50), default="active")
+    user_sessions = db.relationship("UserSession", backref="moderator", lazy=True)
+
+    def __repr__(self):
+        return f"<Moderator {self.fullname}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "fullname": self.fullname,
+            "email": self.email,
+            "is_super_admin": self.is_super_admin,
+            "status": self.status
+        }
 
     @staticmethod
     def validate_email(email):
-        return re.match(r"[^@]+@[^@]+\.[^@]+", email)
-
-    def __repr__(self):
-        return f"<User {self.username}>"
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if re.fullmatch(regex, email):
+            return True
+        return False
 
 
 class UserSession(db.Model):
     __tablename__ = "user_sessions"
     id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
-    user_id = db.Column(db.String(50), db.ForeignKey("users.id"), nullable=False)
+    mod_id = db.Column(db.String(50), db.ForeignKey("moderators.id"), nullable=False)
     otp = db.Column(db.String(6), nullable=True)
     token = db.Column(db.String(255), nullable=True)
     otp_expires_at = db.Column(db.DateTime, nullable=True)
     token_expires_at = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
-        return f"<UserSession {self.user_id}>"
+        return f"<UserSession {self.mod_id}>"
 
 
-class Relationship(db.Model):
+class Spouse(db.Model):
+    __tablename__ = "spouse"
     id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
-    person_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
-    partner_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    husband_1 = db.Column(db.String(50), db.ForeignKey('member.id'))
+    wife_2 = db.Column(db.String(50), db.ForeignKey('member.id'))
+    other_spouses = db.relationship("OtherSpouse", backref="spouse", lazy=True)
+    children = db.relationship("Child", backref="spouse", lazy=True)
+
+
+class OtherSpouse(db.Model):
+    __tablename__ = "other_spouse"
+    id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
+    member_id = db.Column(db.String(50), db.ForeignKey('member.id'))
+    member_related_to = db.Column(db.String(50), db.ForeignKey('member.id'))
     relationship_type = db.Column(SQLAlchemyEnum(RelationshipType))
+    spouse_id = db.Column(db.String(50), db.ForeignKey('spouse.id'))
 
 
 class Child(db.Model):
+    __tablename__ = "child"
     id = db.Column(db.String(50), primary_key=True, default=hex_uuid)
-    parent_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
-    child_id = db.Column(db.String(50), db.ForeignKey('users.id'), nullable=False)
+    member_id = db.Column(db.String(50), db.ForeignKey('member.id'))
+    spouse_id = db.Column(db.String(50), db.ForeignKey('spouse.id'))
     child_type = db.Column(SQLAlchemyEnum(ChildType))
 
 
-def create_user(email, password, first_name, last_name,
-                gender, img_str, is_super_admin=False, family_name=None, phone_number=None,
-                dob=None, status=None, deceased_at=None):
-    user = User(email=email, password=password, first_name=first_name,
-                last_name=last_name, gender=gender, img_str=img_str,
-                is_super_admin=is_super_admin, family_name=family_name,
-                phone_number=phone_number, dob=dob, status=status, deceased_at=deceased_at)
-    db.session.add(user)
+def create_user(email, password):
+    mod = Moderators(email=email, password=password)
+    db.session.add(mod)
     db.session.commit()
-    return user
+    return mod
 
 
 def verify_user_login(email, password):
-    user = User.query.filter_by(email=email).first()
-    if user and hasher.verify(password, user.password):
-        return user
+    mod = Moderators.query.filter_by(email=email).first()
+    if mod and hasher.verify(password, mod.password):
+        return mod
     return False
 
 
-
-def create_otp_token(user_id, otp=None, token=None):
+def create_otp_token(mod_id, otp=None, token=None):
     if otp:
-        user_session = UserSession.query.filter_by(user_id=user_id).first()
+        user_session = UserSession.query.filter_by(mod_id=mod_id).first()
         if user_session:
             user_session.otp = otp
             user_session.otp_expires_at = datetime.now() + timedelta(minutes=10)
             db.session.commit()
         else:
-            user_session = UserSession(user_id=user_id, otp=otp,
+            user_session = UserSession(mod_id=mod_id, otp=otp,
                                        otp_expires_at=datetime.now() + timedelta(minutes=10))
             db.session.add(user_session)
             db.session.commit()
         return user_session
     elif token:
-        user_session = UserSession.query.filter_by(user_id=user_id).first()
+        user_session = UserSession.query.filter_by(mod_id=mod_id).first()
         if user_session:
             user_session.token = token
             user_session.token_expires_at = datetime.now() + timedelta(minutes=10)
             db.session.commit()
         else:
-            user_session = UserSession(user_id=user_id, token=token,
+            user_session = UserSession(mod_id=mod_id, token=token,
                                        token_expires_at=datetime.now() + timedelta(minutes=10))
             db.session.add(user_session)
             db.session.commit()
@@ -188,14 +196,13 @@ def create_otp_token(user_id, otp=None, token=None):
     return None
 
 
-
-def get_user_by_email(email):
-    user = User.query.filter_by(email=email.lower()).first()
+def get_mod_by_email(email):
+    user = Moderators.query.filter_by(email=email.lower()).first()
     return user
 
 
 def valid_email(email):
-    return User.validate_email(email)
+    return Moderators.validate_email(email)
 
 
 def create_family_name(name):
@@ -248,54 +255,18 @@ def get_family_users(family_id):
     return [user.to_dict() for user in users]
 
 
-
 # Update user
-def update_user(user_id,  **kwargs):
-    user = User.query.filter_by(id=user_id).first()
-    if not user:
+def update_user(mod_id,  **kwargs):
+    mod = Moderators.query.filter_by(id=mod_id).first()
+    if not mod:
         return False
     
     # Update user attributes with provided keyword arguments
-    user.first_name = kwargs.get("first_name") or user.first_name
-    user.last_name = kwargs.get("last_name") or user.last_name
-    user.password = kwargs.get("password") or user.password
-    user.gender = kwargs.get("gender") or user.gender
-    user.img_str = kwargs.get("img_str") or user.img_str
-    user.status = kwargs.get("status") or user.status
-    user.email = kwargs.get("email") or user.email
-    
-    
-    # Handle date of birth conversion
-    dob_input = kwargs.get("dob")
-    if dob_input:
-        try:
-            user.dob = datetime.strptime(dob_input, '%d-%m-%Y').date()  # Convert to date
-        except ValueError:
-            return "Invalid date format. Please use DD-MM-YYYY."
-    else:
-        user.dob = user.dob  # Keep existing value if no new value is provided
-
-    user.deceased_at = kwargs.get("deceased_at") or user.deceased_at
-    user.phone_number = kwargs.get("phone_number") or user.phone_number
-    user.family_name = kwargs.get("family_name") or user.family_name
-    user.family_id = kwargs.get("family_id") or user.family_id
+    mod.password = kwargs.get("password") or mod.password
+    mod.email = kwargs.get("email") or mod.email
+    mod.is_super_admin = kwargs.get("is_super_admin") or mod.is_super_admin
     # hash password
-    user.password = hasher.hash(user.password)
-    
-    db.session.commit()
-    # return user
-    users = User.query.filter(User.family_name == family_id).all()
-    return [user.to_dict() for user in users]
-
-
-
-
-# Delete users by id
-def delete_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
-    if not user:
-        return False
-    db.session.delete(user)
+    mod.password = hasher.hash(mod.password)
     
     db.session.commit()
     return True
