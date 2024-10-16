1a7a346c33bfa5b846ffed3f4b8db481a353953c
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-16 15:51:47 +0100

    Update __init__.py

--- app_config/__init__.py
+++ app_config/__init__.py
@@ -10,9 +10,9 @@ from models import Member, ModSession, Spouse, OtherSpouse, Child, Moderators
 
 def create_app(config_name="development"):
     app = Flask(__name__)
-    
+
     app.config.from_object(config_obj[config_name])
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
@@ -33,9 +33,7 @@ def create_app(config_name="development"):
     def not_found(error):
         print("error: ", error)
         return return_response(
-            HttpStatus.NOT_FOUND,
-            status=StatusRes.FAILED,
-            message="Endpoint not found"
+            HttpStatus.NOT_FOUND, status=StatusRes.FAILED, message="Endpoint not found"
         )
 
     @app.errorhandler(405)
@@ -44,7 +42,7 @@ def create_app(config_name="development"):
         return return_response(
             HttpStatus.METHOD_NOT_ALLOWED,
             status=StatusRes.FAILED,
-            message="Method not allowed"
+            message="Method not allowed",
         )
 
     @app.errorhandler(500)
@@ -53,17 +51,41 @@ def create_app(config_name="development"):
         return return_response(
             HttpStatus.INTERNAL_SERVER_ERROR,
             status=StatusRes.FAILED,
-            message="Network Error"
+            message="Network Error",
         )
-        
+
     # user loader
     @jwt.user_lookup_loader
     def user_lookup_callback(_jwt_header, jwt_data):
         user_id = jwt_data["sub"]
         return Moderators.query.get(user_id)
-    
+
+    @jwt.expired_token_loader
+    def my_expired_token_callback(jwt_header, jwt_payload):
+        return return_response(
+            HttpStatus.UNAUTHORIZED,
+            status=StatusRes.FAILED,
+            message="Token Expired",
+        )
+
+    @jwt.invalid_token_loader
+    def my_invalid_token_callback(jwt_header, jwt_payload):
+        return return_response(
+            HttpStatus.UNAUTHORIZED,
+            status=StatusRes.FAILED,
+            message="Invalid Token",
+        )
+
+    @jwt.unauthorized_loader
+    def my_unauthorized_callback(jwt_header, jwt_payload):
+        return return_response(
+            HttpStatus.UNAUTHORIZED,
+            status=StatusRes.FAILED,
+            message="Unauthorized",
+        )
+
     app.register_blueprint(AuthBlp, url_prefix="/api/v1")
     app.register_blueprint(AccountBlp, url_prefix="/api/v1")
     app.register_blueprint(CloudinaryBlp, url_prefix="/api/v1")
-    
+
     return app
